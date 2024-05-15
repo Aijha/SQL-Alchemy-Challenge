c4a2b5ecb0d278653c062908105e01b3d400b4fb
Aijha <143448902+Aijha@users.noreply.github.com>
2024-05-15 12:32:59 -0700

    app.py update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -57,4 +57,85 @@ def welcome():
         f"&nbsp&nbsp&nbsp&nbsp&nbsp termperatures for EVERY day starting with the start date and ending with the last date in the data.<br/>"
     )
 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    max_date = dt.datetime(2017,8,23)
+    min_date = max_date - dt.timedelta(days=366)
+
+    session = Session(engine)
+    results_df = pd.DataFrame(session.query(measurement.date,measurement.prcp).filter(measurement.date >= min_date, measurement.date <= max_date).all())
+    session.close()
+
+    results_dict = {results_df['date'][i]:results_df['prcp'][i] for i in range(len(results_df['date']))}
+
+    return jsonify(results_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    station_result = session.query(station.station).all()
+    session.close()
+
+    station_list = [station[0] for station in station_result]
+
+    return jsonify(station_list)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    max_date = dt.datetime(2017,8,23)
+    min_date = max_date - dt.timedelta(days=366)
+
+    session = Session(engine)
+    temp_results = session.query(measurement.date,measurement.tobs).filter(measurement.date >= min_date, measurement.date <= max_date, measurement.station == 'USC00519281').all()
+    session.close()
+
+    temp_results_df = pd.DataFrame(temp_results,columns=['date','tobs'])
+
+    temp_list = list(temp_results_df['tobs'])
+
+    return jsonify(temp_list)
+
+@app.route("/api/v1.0/<start_date>")
+def temps_w_start(start_date):
+
+    session = Session(engine)
+    data = session.query(func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+                   filter(measurement.date>=start_date).all()
+    session.close()
+
+    return jsonify(list(data[0]))
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def temps_w_start_end(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    data = session.query(func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+                   filter(measurement.date>=start, measurement.date<=end).all()
+    session.close()
+
+    return jsonify(list(data[0]))
+
+
+
+@app.route("/api/v1.0/all_dates/<start_date>")
+def temps_w_start_details(start_date):
+
+    # This endpoint gets the min, avg, and max temp for EVERY date after the <start_date> specified.
+
+    session = Session(engine)
+    data = session.query(measurement.date,func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+                   filter(measurement.date>=start_date).\
+                   group_by(measurement.date).all()
+    session.close()
+
+    data_dict = {d[0]:[d[1],d[2],d[3]] for d in data}
+
+    return jsonify(data_dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 #################################################
